41f6f2d5baa96265b1660d7ef83c02a4608c87d0
Nigel Huang <28766663+nigel5@users.noreply.github.com>
2018-12-01 11:04:59 -0500

    add Folders

---
+++ src/components/item-card/ItemCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ItemCard.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ItemCard">
+        Item Card
+      </div>
+    );
+  }
+}
+
+export default App;
