6de5a59cc73faef46e669c30bec96c7c63b3e508
Nigel Huang <28766663+nigel5@users.noreply.github.com>
2018-12-15 23:29:17 -0500

    Add color to item status test
    Add margin to item cards

--- src/components/item-card/ItemCard.js
+++ src/components/item-card/ItemCard.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './ItemCard.css';
-import { Card, CardText, CardBody,
-	  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import {
+Card, CardText, CardBody, CardTitle, CardSubtitle,
+CardImg, Button} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class ItemCard extends Component {
+	constructor(props) {
+		super(props);
+
+		switch (this.props.status) {
+			case "lost":
+				this.statusTextColor = "#FF0000";
+				break;
+			case "returned":
+				this.statusTextColor = "#008000";
+				break;
+			case "deleted":
+				this.statusTextColor = "#000000";
+				break;
+			case "auctioned":
+				this.statusTextColor = "#A52A2A";
+				break;
+			case "donated":
+				this.statusTextColor = "#800080";
+				break;
+			default:
+				this.statusTextColor = "#000000";
+		}
+	}
+
   render() {
     return (
-    	    <div>
+    	    <div className="ItemCard">
     	      <Card>
+							<CardImg top src={this.props.imageURL} alt="Item image"/>
     	        <CardBody>
     	          <CardTitle>{this.props.name}</CardTitle>
-    	          <CardSubtitle>{this.props.comments}</CardSubtitle>
-    	        </CardBody>
-    	        <img width="100%" src={this.props.imageURL} />
-    	        <CardBody>
-    	          <CardText>Card Test</CardText>
+    	          <CardSubtitle style={{color: this.statusTextColor}}>
+									{this.props.status}
+								</CardSubtitle>
+    	          <CardText>{this.props.comment}</CardText>
     	          <Button color="primary">More Info</Button>
     	        </CardBody>
     	      </Card>
