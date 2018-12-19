00ffbc751e63d9e05d4c20bf0402f1755795106b
Nigel Huang <28766663+nigel5@users.noreply.github.com>
2018-12-18 22:17:58 -0500

    feat(items): Add ability to edit and archive items

---
+++ src/components/edit-item/EditItem.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Col, Form, FormGroup, Label, Input} from 'reactstrap';
+
+export default class EditItem extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        name: "",
+        status: "",
+        comment: "",
+        imageURL: "",
+        id: ""
+      };
+
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+      this.setState({
+        name: this.props.name,
+        status: this.props.status,
+        comment: this.props.comment,
+        imageURL: this.props.imageURL,
+        _id: this.props.id
+      });
+    }
+    getFormData() {
+      return {
+        ...this.state
+      };
+    }
+
+    handleChange(event) {
+      this.setState({[event.target.name]: event.target.value});
+    }
+
+    render() {
+        return (
+            <Form>
+                <FormGroup row>
+                    <Label for="nameInput" sm={2}>Name</Label>
+                    <Col sm={10}>
+                        <Input type="text" name="name" id="nameInput" placeholder="Name of item"
+                               value={this.state.name}
+                                 onChange={this.handleChange}
+                                 invalid={!this.state.name}/>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="selectStatus" sm={2}>Select Status</Label>
+                    <Col sm={10}>
+                        <Input type="select" name="status" id="statusSelector"
+                               value={this.state.status}
+                               onChange={this.handleChange}>
+                            <option>Lost</option>
+                            <option>Returned</option>
+                            <option>Donated</option>
+                            <option>Auctioned</option>
+                            <option>Archived</option>
+                        </Input>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="commentBox" sm={2}>Description{'\u00A0'}</Label>
+                    <Col sm={10}>
+                        <Input type="textarea" name="comment" id="commentBox"
+                               value={this.state.comment}
+                               onChange={this.handleChange}/>
+                    </Col>
+                </FormGroup>
+                {
+                // <FormGroup row>
+                //     <Label for="fileSelector" sm={2}>Image</Label>
+                //     <Col sm={10}>
+                //         <Input type="file" name="imageURL" id="imageFilePicker"
+                //                value={this.state.imageURL}
+                //                onChange={this.handleChange}/>
+                //         <FormText color="muted">
+                //             Select an image to be displayed for this item
+                //         </FormText>
+                //     </Col>
+                // </FormGroup>
+                }
+            </Form>
+        );
+    }
+}
